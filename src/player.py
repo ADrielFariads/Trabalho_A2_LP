37fce7b1d34c26ccc51ded59fc2e1452e3196e28
adriel <adriel25dias@gmail.com>
2024-10-31 15:49:06 -0300

    correct the direction vector

--- src/player.py
+++ src/player.py
@@ -5,7 +5,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         #animation
         self.image = pygame.image.load("assets/images/testimage01.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (50, 50)).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (100, 100)).convert_alpha()
         self.position = pygame.math.Vector2(posx, posy)
         self.rect = self.image.get_rect(center = (posx, posy))
 
@@ -18,13 +18,13 @@ class Player(pygame.sprite.Sprite):
         direction = pygame.math.Vector2(0,0)
 
         if keys[pygame.K_a]:
-            direction = -1
+            direction.x = -1
         if keys[pygame.K_d]:
-            direction = 1
+            direction.x = 1
         if keys[pygame.K_w]:
-            direction = -1
+            direction.y = -1
         if keys[pygame.K_s]:
-            direction = 1
+            direction.y = 1
 
         if direction.length() > 0:
             direction = direction.normalize()
