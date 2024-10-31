1a31b8360effa0ed694c8dbfbf78477e1fa3c08d
adriel <adriel25dias@gmail.com>
2024-10-31 11:14:03 -0300

    initial config of main file

--- src/main.py
+++ src/main.py
@@ -1,2 +1,21 @@
 import pygame
 
+#initial setup
+
+pygame.init()
+window_width, window_height = 1280, 720
+display_surface = pygame.display.set_mode((window_width, window_height))
+
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    clock.tick(60)
+
+pygame.quit()
+
+
