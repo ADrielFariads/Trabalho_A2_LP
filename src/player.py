11485f0c0f73825c5333a5fd2f08cef3821ddc7d
adriel <adriel25dias@gmail.com>
2024-11-02 09:45:19 -0300

    small changes

--- src/player.py
+++ src/player.py
@@ -75,7 +75,7 @@ class Player(pygame.sprite.Sprite):
         
         if action == "walk":
             self.current_action = "walk"
-            self.sprite_sheet = pygame.image.load("assets\images\Player\Run1.png")
+            self.sprite_sheet = pygame.image.load("assets/images/Player/Run1.png")
             self.frames = self.load_frames(self.sprite_sheet, 6)
 
         
