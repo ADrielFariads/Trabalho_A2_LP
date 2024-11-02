7231dce03750872a5f633d48d381f2ff1e325546
adriel <adriel25dias@gmail.com>
2024-11-02 11:15:32 -0300

    created the module tools.py

--- src/background.py
+++ src/background.py
@@ -1,18 +1,6 @@
 import pygame
 import os
 
-import pygame.locals
-
-# loading the images
-def load_images(folder):
-    images = []
-    for each in os.listdir(folder):
-        image_path = os.path.join(folder, each)
-        image = pygame.image.load(image_path)
-        images.append(image)
-    return images
-
-
 
 class Scenario(pygame.sprite.Sprite):
    pass
