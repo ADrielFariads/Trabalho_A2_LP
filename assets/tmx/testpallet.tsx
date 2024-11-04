e6d656d838bdca730368a819eb0e17c91c3653d7
adriel <adriel25dias@gmail.com>
2024-11-04 18:19:47 -0300

    deleted some files of background

--- assets/tmx/testpallet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="testpallet" tilewidth="32" tileheight="32" tilecount="1222" columns="47">
- <image source="testpallet.png" width="1522" height="857"/>
-</tileset>
